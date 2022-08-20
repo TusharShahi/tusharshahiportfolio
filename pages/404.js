461d8d666e34409efdb63fecb3829cb716c09504
Tushar Shahi <tusharshahi@Tushars-MacBook-Pro.local>
2022-08-20 22:53:49 +0530

    removed 404

--- pages/404.js
+++ pages/404.js
@@ -6,13 +6,4 @@ const Custom404 = () => {
   );
 };
 
-export const getServerSideProps = async ({ req }) => {
-  const theme = req.cookies.themeSwitch ?? "LIGHT";
-  return {
-    props: {
-      theme
-    } // will be passed to the page component as props
-  };
-};
-
 export default Custom404;
